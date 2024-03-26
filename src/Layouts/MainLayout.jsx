c3264c3e607ffe6ed1ea650c950f8a1baa3966c9
Muhammad <shalauddinahmedshipon@gmail.com>
2024-03-26 15:57:10 -0800

    Environment Setup done

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+
+
+const MainLayout = () => {
+  return (
+    <div>
+      <ul><li>navbar</li>
+      <li>outlet-
+
+       <div>home, listed books , pages to read</div> 
+
+      </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MainLayout;
