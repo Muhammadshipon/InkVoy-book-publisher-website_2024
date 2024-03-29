4e8f244b804d22215b147ad78cc5e6a319bccb19
Muhammad <shalauddinahmedshipon@gmail.com>
2024-03-28 21:55:01 -0800

    Footer added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navber from "../Components/Navber";
+import Footer from "../Components/Footer";
 
 
 const MainLayout = () => {
   return (
+    <div>
     <div className="max-w-7xl mx-auto">
      <Navber></Navber>
 
       
 <Outlet></Outlet>
+</div>
+<Footer></Footer>
       
     </div>
   );
